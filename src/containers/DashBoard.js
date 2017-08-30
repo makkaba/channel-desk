d133a38c420a91e446a8d7de5ecd509a0fbbb1b8
Jeff Chung <drifterz303@gmail.com>
2017-08-31 00:47:38 +0900

    차트를 컴포넌트로 분리시킨다

--- src/containers/DashBoard.js
+++ src/containers/DashBoard.js
@@ -1,54 +1,16 @@
 import React, { Component } from 'react';
 import * as roomAPI from '../services/rooms';
-import * as waterAPI from '../services/water';
+
 import Navbar from '../components/Navbar';
+import Chart from '../components/Chart';
 import { List, ListItem, ListItemText, Divider } from 'material-ui';
-import { LineChart, Line, XAxis, YAxis, 
-        Tooltip, Legend, CartesianGrid, ResponsiveContainer } from 'recharts';
-import * as _ from 'lodash';
-import Moment from 'moment';
-import ko from 'moment/locale/ko';
-Moment.locale("ko", ko);
 
 class DashBoard extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            data: []
-        };
-        
-    }
-    componentDidMount(){
-        
-        waterAPI.getMyList('kdfjsdlkflkj').then((result)=> {
-            var data = Object.values(result.data).map((item)=> ({
-                name: Moment(item.created_at).format("YYYY-MM-DD"),
-                water: item.quantity,
-                fromNow: Moment(item.created_at).fromNow()
-            }));
-            _.reverse(data);
-            var groups = _.groupBy(data, function(item){
-                return item.name;
-            });
-            var filteredData = _.map(groups, function(value, key){
-                var sum = 0;
-                var fromNow = '';
-                value.forEach(function(cup){
-                    sum += cup.water;
-                    fromNow = cup.fromNow;
-                });
-                return {
-                    name: key,
-                    water: sum,
-                    fromNow: fromNow
-                };
-            });
-            this.setState({data: filteredData});
-        });
-    }
+    
+    
     
     render(){
-        const { data } = this.state;
+        
         return(
           <div>
             <Navbar />
@@ -68,18 +30,7 @@ class DashBoard extends Component{
                   </ListItem>
                 </List>
               </div>
-                  <div style={{flex: "1 1 auto", margin: "5%"}}>
-                      <ResponsiveContainer>
-                          <LineChart data={data}>
-                              <XAxis dataKey="fromNow"/>
-                              <YAxis/>
-                              <CartesianGrid strokeDasharray="3 3"/>
-                              <Tooltip/>
-                              <Legend />
-                              <Line type="monotone" dataKey="water" stroke="#8884d8" activeDot={{r: 8}} />
-                          </LineChart>
-                      </ResponsiveContainer>
-                  </div>
+              <Chart />
             </div>
           </div>
             
